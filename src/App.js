538423f342580fc951ee038849a72315cd553b40
saloni suryavanshi <salonisuryavanshi123>
2023-04-08 22:06:47 +0530

    progressBar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+//1. Import Area
+import { useState } from 'react';
+import swal from 'sweetalert';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+//2. Defination Area
+function App() {  
+  //2.1 Hook Area
+  const [file,setFile] = useState('');
+  const [uploadPercentage, setUploadPercentage] = useState(0);
+
+
+  //2.2 Function Defination Area
+  let handleChange = (e)=>{
+    //alert("okokok")
+    let img = document.getElementById("imgPreview")
+    console.log(e.target.files[0]);
+    let file = e.target.files[0];
+    setFile(file);
+    if(file.type === 'image/jpeg' || file.type === 'image/png'){
+     // alert('I can upload the file')
+      img.src = URL.createObjectURL(file);//'https://www.datocms-assets.com/45470/1631110818-logo-react-js.png';
+    }else{
+      alert('Please upload image only');
+    }
+  }
+
+  let handleUpload = (e)=>{
+    //alert("hye")
+
+    const formData = new FormData();
+    formData.append('files', file);
+
+    axios.post('http://localhost:1337/api/upload', formData,{
+      headers:{
+        "Content-Type" : "multipart/form-data",
+      },
+      onUploadProgress : (progressEvent) => {
+        var progress = parseInt(Math.round((progressEvent.loaded * 100) / progressEvent.total));
+        console.log(progress);
+        setUploadPercentage(progress);
+        // Update state here
+        //onUploadProgress(progress);
+        if(progress === 100){
+          //setUploadPercentage(0);
+          swal("Good job!", "Image uploaded successfully!", "success");
+        }
+      }
+    });
+
+  }
+
+
+  //2.3 Return Statement
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form>
+          <input type="file" name="files" onChange={(e)=>{ handleChange(e) }} />
+          <input type="button" value="Upload" onClick={handleUpload} />
+        </form>
+
+        <br />
+        <img src="" id="imgPreview" />
+        {
+          uploadPercentage > 0 &&
+        <div className="progress w-50 offset-3" role="progressbar" aria-label="Success striped example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+          <div className="progress-bar progress-bar-striped bg-success" style={{width: uploadPercentage+"%"}}></div>
+        </div>
+        }
     </div>
   );
 }
 
+//3. Export Area
 export default App;
